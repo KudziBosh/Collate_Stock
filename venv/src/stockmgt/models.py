1a87441c2d7e2fcf1d4c5344ebc4a6bad14e9740
KudziBosh <62004957+KudziBosh@users.noreply.github.com>
2022-12-26 20:25:28 +0200

    3 hours 9 mins

--- venv/src/stockmgt/models.py
+++ venv/src/stockmgt/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 
+
+category_choice = (
+		('Belts', 'Belts'),
+		('Shoes', 'Shoes'),
+		('Trousers', 'Trousers'),
+	)
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=50, blank=True, null=True)
+	def __str__(self):
+		return self.name
+
 class Stock(models.Model):
-	category = models.CharField(max_length=50, blank=False, null=True)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True,blank=True)
 	item_name = models.CharField(max_length=50, blank=True, null=True)
 	quantity = models.IntegerField(default='0', blank=True, null=True)
 	receive_quantity = models.IntegerField(default='0', blank=True, null=True)
 	receive_by = models.CharField(max_length=50, blank=True, null=True)
 	issue_quantity = models.IntegerField(default='0', blank=True, null=True)
 	issue_by = models.CharField(max_length=50, blank=True, null=True)
-	issue_to = models.CharField(max_length=50, blank=True, null=True)
+	issue_to = models.CharField(max_length=50, blank=True, null=True) 
 	phone_number = models.CharField(max_length=50, blank=True, null=True)
 	created_by = models.CharField(max_length=50, blank=True, null=True)
 	reorder_level = models.IntegerField(default='0', blank=True, null=True)
 	last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-	export_to_CSV = models.BooleanField(default=False)
+	timestamp=models.DateTimeField(auto_now=False,auto_now_add=True)
+	date=models.DateTimeField(auto_now=False,auto_now_add=False  )
+ 
 
 	def __str__(self):
 		return self.item_name
--- venv/src/stockmgt/views.py
+++ venv/src/stockmgt/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import *
 from .forms import *
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import csv
+from django.contrib import messages
 
 # Create your views here.
 
@@ -15,12 +18,34 @@ def home(request):
 
 
 def list_item(request):
+    form = StockSearchForm(request.POST or None)
     title = 'List of Items'
     queryset = Stock.objects.all()
     context = {
+        "form": form,
         "title": title,
         "queryset": queryset,
     }
+    if request.method == 'POST':
+        queryset = Stock.objects.filter(  # category__icontains=form['category'].value(),
+            item_name__icontains=form['item_name'].value(
+            )
+        )
+        if form['export_to_CSV'].value() == True:
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename="List of stock.csv"'
+            writer = csv.writer(response)
+            writer.writerow(['CATEGORY', 'ITEM NAME', 'QUANTITY'])
+            instance = queryset
+            for stock in instance:
+                writer.writerow(
+                    [stock.category, stock.item_name, stock.quantity])
+            return response
+        context = {
+            "form": form,
+            "title": title,
+            "queryset": queryset,
+        }
     return render(request, "list_item.html", context)
 
 
@@ -35,3 +60,27 @@ def add_items(request):
         "title": "Add Item",
     }
     return render(request, "add_item.html", context)
+
+
+def update_items(request, pk):
+    queryset = Stock.objects.get(id=pk)
+    form = StockUpdateForm(instance=queryset)
+    if request.method == 'POST':
+        form = StockUpdateForm(request.POST, instance=queryset)
+        if form.is_valid():
+            form.save()
+            return redirect('/list_item')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'add_item.html', context)
+
+
+def delete_items(request, pk):
+    queryset = Stock.objects.get(id=pk)
+    if request.method == 'POST':
+        queryset.delete()
+        messages.success(request, 'Successfully Deleted')
+        return redirect('/list_item')
+    return render(request, 'delete_items.html')
